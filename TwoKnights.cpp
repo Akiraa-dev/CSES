3239e76e6c16ebb007a31f860cf993c87abea82b
Huy Dep Trai <huyxxgd@gmail.com>
2023-05-29 16:12:39 +0700

    first commit

---
+++ TwoKnights.cpp
@@ -0,0 +1,35 @@
+// author: akira
+#include <bits/stdc++.h>
+#define endl '\n'
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+#define cint(x) int(x - '0')
+#define cchar(x) char(x + '0')
+#define pb push_back
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define llll pair<long long, long long>
+#define gcd(x, y) __gcd(x, y)
+#define lcm(x, y) x/__gcd(x, y)*y
+#define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+typedef long long ll;
+typedef long double ldb;
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 7;
+const long long INFLL = (long long)1e18 + 7;
+void solve() {
+    ll n; cin >> n;
+    for(ll i = 1; i <= n; i++) {
+        ll res = (i * i * (i * i - 1)) / 2 - (4 * (i - 2) * (i - 1));
+        cout << res << endl;
+    }
+}
+int main() {
+    fastIO
+//    freopen("test.inp", "r", stdin);
+//    freopen("test.out", "w", stdout);
+    solve();
+    return 0;
+}
